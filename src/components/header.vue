bd4659d2d2c34a4936da7b2c4aed04341b2c4420
navyFox <lina.666@list.ru>
2021-10-11 21:54:37 +0300

    создала сборку

---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">Главная</router-link>
+      <router-link to="/">о театре</router-link>
+      <router-link to="/">Наши постановки</router-link>
+      <router-link to="/">партнеры театра</router-link>
+      <router-link to="/">контакты</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "header",
+};
+</script>
+
+<style scoped></style>
