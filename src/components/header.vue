350aeb8d9de5e7303b6e3cbab520028b47493015
navyFox <lina.666@list.ru>
2021-10-12 14:35:59 +0300

    добавила header.vue

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    <nav>
+  <header class="grid header">
+    <nav class="header__nav column-9 text text--uppercase text--middle">
       <router-link to="/">Главная</router-link>
       <router-link to="/">о театре</router-link>
       <router-link to="/">Наши постановки</router-link>
       <router-link to="/">партнеры театра</router-link>
       <router-link to="/">контакты</router-link>
     </nav>
-  </div>
+    <div class="column-3 header__tel">
+      <a href="tel:+74951234567" class="text">+7 495 123-45-67</a>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -16,4 +19,26 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+@import "src/assets/scss/system";
+
+.header {
+  padding-top: 50px;
+
+  &__nav {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__tel {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    a {
+      border-bottom: 2px $color-gold solid;
+      padding-bottom: 5px;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="">
-    <div class="wrapper">
-      <p class="text">hello world</p>
-    </div>
+  <div class="home">
+    <section class="section section--main">
+      <div class="wrapper">
+        <Header />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import Header from "../components/header";
 export default {
   name: "Home",
-  components: {},
+  components: { Header },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "src/assets/scss/system";
+
+.home {
+  background: $color-black;
+  min-height: 100vh;
+}
+</style>
